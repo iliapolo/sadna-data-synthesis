b3fdf3dd1b47878b591b257e32dcf6d2cd71125e
iliapolo <Eli.polonsky@gmail.com>
2013-07-06 13:02:31 +0300

    added message bundle and controller class

---
+++ relwiki/src/main/java/org/sadnatau/relwiki/controller/Controller.java
@@ -0,0 +1,11 @@
+package org.sadnatau.relwiki.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Elip
+ * Date: 7/6/13
+ * Time: 1:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Controller {
+}
