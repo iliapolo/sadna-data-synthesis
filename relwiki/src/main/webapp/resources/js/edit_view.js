ec052383fdb350ae5cf44e64d33b8cdafabfdcb9
iliapolo <elip@gigaspaces.com>
2013-09-07 21:19:07 +0300

    implement edit page client side

---
+++ relwiki/src/main/webapp/resources/js/edit_view.js
@@ -0,0 +1,29 @@
+function savePage() {
+
+    var pageTitle = document.getElementById('page_title').innerText;
+    var edit_author_name = document.getElementById('edit_author_name').value;
+    var edit_content = document.getElementById('page_content').value;
+
+    var pageEdit = {
+        "author" : edit_author_name,
+        "content" : edit_content
+    }
+
+    var jsonPageEdit = JSON.stringify(pageEdit);
+
+    $.ajax({
+        type: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        url: "../edit/" + pageTitle,
+        data: jsonPageEdit,
+        dataType: 'json',
+        error:   function(jqXHR, textStatus, errorThrown) {
+            alert("Error, status = " + textStatus + ", " +
+                "error thrown: " + errorThrown
+            );
+        }
+    });
+}
